8d1b18ec6d3778ad8447e92480ab524395b5a9e8
Suider17 <suider17@gmail.com>
2025-02-22 17:01:52 -0600

    adding settings in main.js

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
+class MainScene extends Phaser.Scene {
+  preload() {
+    this.load.setBaseURL("https://cdn.phaserfiles.com/v385");
+  }
+  create() {}
+}
 
 const config = {
+  type: Pasher.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: function () {
+      this.preload.iiage("logo", "../public/vite.svg");
+    },
+    create: function () {
+      this.add.image(400, 300, "logo");
+    },
+  },
+};
 
-  type: Pasher.AUTO
-
-
-
-
-}
+const game = new Phaser.Game(config);
